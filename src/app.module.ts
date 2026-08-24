931b06e03be065533dc59cc5214b90936d06353b
root <root@cod-2-lussiernat.lan>
2026-08-24 22:02:40 +0000

    init buildings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
+import { BuildingsModule } from './buildings/buildings.module';
 
 @Module({
 
-  imports: [HealthModule]
+  imports: [HealthModule, BuildingsModule]
 })
 export class AppModule {}
