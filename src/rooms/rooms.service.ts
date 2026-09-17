50b338d501933aafaa42db932c91632704a13b3f
Milleboy2007 <2484435@cegepmv.ca>
2026-09-17 21:32:44 +0000

    maj room service

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,44 +1,39 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
-import { Room } from './entities/room.entity';
+import { Room } from './schemas/room.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class RoomsService {
 
-  private Rooms: Room[] = [];
+  constructor(
+    @InjectModel(Room.name) private readonly roomModel: Model<Room>
+  ){}
 
-  create(createRoomDto: CreateRoomDto) {
-    const {code,  buildingId, floor, type, capacity} = createRoomDto
-    const newRoom = new Room(code,  buildingId, floor, type, capacity);
-    Object.assign(newRoom, createRoomDto)
-    this.Rooms.push(newRoom);
-    return newRoom;
+  async create(createRoomDto: Partial<Room>): Promise<Room> {
+    return this.roomModel.create(createRoomDto);
   }
 
-  findAll() {
-    return this.Rooms;
+  async findAll() {
+    return this.roomModel.find().exec();
   }
 
-  findOne(id: string) {
-    var toFound = this.Rooms.find(r => r.id === id);
+  async findOne(id: string): Promise<Room> {
+    var toFound = await this.roomModel.findById(id).exec();
     if(!toFound) throw new NotFoundException('Aucun batiment de cette id');
     return toFound;
   }
 
-  update(id: string, updateRoomDto: UpdateRoomDto) {
-    const toChange = this.findOne(id);
-    Object.assign(toChange, updateRoomDto);
-    toChange.updatedAt = new Date();
-    
-    return toChange;
+  async update(id: string, updateRoomDto: Partial<Room>): Promise<Room> {
+    var toUpdate = await this.roomModel.findByIdAndUpdate(id, updateRoomDto, {new: true}).exec();
+    if(!toUpdate) throw new NotFoundException(`Aucun room de cette id: ${id}`);
+    return toUpdate;
   }
 
-  remove(id: string): void {
-    const index: number = this.Rooms.findIndex((r: Room) => r.id === id);
-
-    if(index == -1) throw new NotFoundException(`Room avec id: ${id}, non trouver`);
-
-    this.Rooms.splice(index, 1);
+  async remove(id: string): Promise<Room> {
+      var toDelete = await this.roomModel.findByIdAndDelete(id).exec();
+      if(!toDelete) throw new NotFoundException(`Aucun room de cette id: ${id}`);
+      return toDelete;
   }
 }
